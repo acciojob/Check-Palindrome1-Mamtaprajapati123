66ed2c635ebe2c7fcabc429ca87c32f151594f22
Mamtaprajapati123 <124058185+Mamtaprajapati123@users.noreply.github.com>
2023-05-12 12:22:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 // complete the given function
 
 function palindrome(str){
-
+str = str.toLowerCase().replace(/[\W_]/g, '');
+  
+  // check if the reversed string is equal to the original string
+  return str === str.split('').reverse().join('');
 }
 module.exports = palindrome
